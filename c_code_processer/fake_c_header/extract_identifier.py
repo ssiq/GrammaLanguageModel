6ad14d61dfe6ae2197f4e70ce442cdfd892d7c1c
lfsoftware13 <lfsoftware13@gmail.com>
2018-05-13 22:37:55 +0800

    extrace ids, types of fake c header

--- c_code_processer/fake_c_header/extract_identifier.py
+++ c_code_processer/fake_c_header/extract_identifier.py
@@ -9,20 +9,25 @@ def extract_identifier(code):
     c_parser = init_pycparser(lexer=BufferedCLex)
     c_parser.parse(code)
     global_scope = c_parser._scope_stack[0]
-    ids = list(global_scope.keys())
+    ids = list(zip(*list(filter(lambda x: not x[1], global_scope.items()))))[0]
+    types = list(zip(*list(filter(lambda x: x[1], global_scope.items()))))[0]
     print(ids)
-    return ids
+    print(types)
+    return ids, types
 
 
 def extract_fake_c_header_identifier():
     file_list = ['math.h', 'stdio.h', 'stdlib.h', 'string.h']
-    res = set()
+    res_ids = set()
+    res_types = set()
     for fp in file_list:
         afp = os.path.join(ROOT_PATH, 'c_code_processer', 'fake_c_header', fp)
         with open(afp) as f:
             text = f.read()
-            res |= set(extract_identifier(text))
-    return list(res)
+            ids, types = extract_identifier(text)
+            res_ids |= set(ids)
+            res_types |= set(types)
+    return res_ids, res_types
 
 
 if __name__ == '__main__':
